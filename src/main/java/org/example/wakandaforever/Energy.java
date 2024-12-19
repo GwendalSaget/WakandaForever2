baa70df79ebc33825ad40f28dabd465b75d9c174
Gwendal20 <gsaget@et.esiea.fr>
2024-12-19 03:33:33 +0100

    Commit

--- src/main/java/org/example/wakandaforever/Energy.java
+++ src/main/java/org/example/wakandaforever/Energy.java
@@ -1,9 +1,3 @@
 package org.example.wakandaforever;
 
-public class Energy{
-    //Menos energia producida cada dia pero muy barrato
-    int FabricaKillmonger;
-    //Fabrica Black Panther -> Mucha ernergia producida pero cuesta mucho a la fabricacion
-    int FabricaShuri;
-    long energy;
-}
+//TODO
--- src/main/java/org/example/wakandaforever/Vibranium.java
+++ src/main/java/org/example/wakandaforever/Vibranium.java
@@ -1,9 +1,3 @@
 package org.example.wakandaforever;
 
-public class Vibranium{
-long vibranium;
-//l'activité principale du Wakanda est le développement de tech de super héros'
-//bien que lucratif cela demande un certain coût en energie électrique
-// extracteur correspond aux nombres de machines pour récup le vibranium
-int extracteur;
-}
+//TODO
